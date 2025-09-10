3430c51d5e7b08cfb0c6b3d0f729cdc8437cc5ea
Guillermo Carrillo <l22440589@mochis.tecnm.mx>
2025-09-10 07:33:54 -0700

    Inicializa proyecto con estructura base para cliente y servidor

--- Cliente/cliente/src/com/mycompany/cliente/Cliente2025.java
+++ Cliente/cliente/src/com/mycompany/cliente/Cliente2025.java
@@ -4,10 +4,19 @@
  */
 package com.mycompany.cliente;
 
+import java.io.*;
+import java.net.Socket;
 /**
  *
  * @author Guillermo
  */
+
 public class Cliente2025 {
-    
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 9090)) {
+            System.out.println("Conectado al servidor.");
+        } catch (IOException e) {
+            System.out.println("No se pudo conectar al servidor: " + e.getMessage());
+        }
+    }
 }
