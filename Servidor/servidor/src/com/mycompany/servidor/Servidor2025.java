d578ee3a4ac1a62583087b88e700827e79128417
Guillermo Carrillo <l22440589@mochis.tecnm.mx>
2025-09-10 07:34:03 -0700

    Inicializa proyecto con estructura base para cliente y servidor

--- Servidor/servidor/src/com/mycompany/servidor/Servidor2025.java
+++ Servidor/servidor/src/com/mycompany/servidor/Servidor2025.java
@@ -3,11 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.servidor;
-
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 /**
  *
  * @author Guillermo
  */
 public class Servidor2025 {
-    
+    public static void main(String[] args) {
+        try (ServerSocket servidor = new ServerSocket(9090)) {
+            System.out.println("Servidor en puerto 9090. Esperando conexiones...");
+            while (true) {
+                Socket socket = servidor.accept();
+                System.out.println("Nuevo cliente conectado: " + socket.getRemoteSocketAddress());
+            }
+        } catch (IOException e) {
+            System.out.println("Error en el servidor: " + e.getMessage());
+        }
+    }
 }
